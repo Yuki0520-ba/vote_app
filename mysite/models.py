11cd1ac0586ac15cdc2a91faaa32eb348b79f104
Yuki0520-ba <>
2020-01-26 13:05:37 +0900

    fix

--- mysite/models.py
+++ mysite/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 class Band(models.Model):
     band_name=models.CharField(verbose_name='バンド名',max_length=30)
-    point=models.DecimalField(
+    point=models.IntegerField(
         verbose_name='点数',
         default=0,
-        max_digits=3, decimal_places=0
+       
     )
     order=models.IntegerField(verbose_name='順番',default=0)
     posted_date=models.DateTimeField(auto_now_add=True)
