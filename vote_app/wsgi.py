209ef61b36c044f2038980b92197d20d714d36df
Yuki0520-ba <>
2020-01-26 10:31:40 +0900

    wisgi fixed

--- vote_app/wsgi.py
+++ vote_app/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vote_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vote_app.settings.heroku')
 
 application = get_wsgi_application()
